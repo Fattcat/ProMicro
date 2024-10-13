c33deafe05bec756a9a434001dab3fc2b806082a
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-10-13 15:43:28 +0200

    Create BetterCode-V2.ino

---
+++ BetterCode-V2.ino
@@ -0,0 +1,20 @@
+// Here GOING TO BE ADDED stuff like :
+// 1. RED, GREEN, ORANGE LEDS Indicators :
+// - RED LED    - If LIGHTS then it Indicates CODE Error or else error
+// - GREEN LED  - If LIGHTS then it Indicates CODE 
+// - ORANGE LED - If LIGHTS then it Indicates CODE Code still in process (Code still running)
+// COMMANDS :
+// - Blink(RED_LED, int times) // Which LED and how many times to blink
+// --> Blink(RED_LED, 3)
+
+// 2. ADDed Function for MOUSE MOVING
+// - void(MOUSE_Move_Left, int pixels)
+// - void(MOUSE_Move_Right, int pixels)
+// - void(MOUSE_Move_Down, int pixels)
+// - void(MOUSE_Move_Up, int pixels)
+// - void(Mouse_Click) // Just click
+// - void(Mouse_Click, int x_position, int y_position) // Click on x, y Position
+
+// And Other ...
+
+// Code
