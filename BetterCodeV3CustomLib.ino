688761504b54ef4feae4f592bcae26b99180b958
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2025-04-21 10:52:10 +0200

    Update BetterCodeV3CustomLib.ino

--- BetterCodeV3CustomLib.ino
+++ BetterCodeV3CustomLib.ino
@@ -1,41 +1,148 @@
 #include <SPI.h>
 #include <SD.h>
-#include <string.h>
-#include "Keyboard.h"
-#include "Mouse.h"
+#include <Keyboard.h>
+#include <Mouse.h>
 
 #define GREEN_LED 3
 #define ORANGE_LED 4
-#define SD_CS 10
+#define SD_CS_PIN 10
+#define FILENAME "inject.txt"
 
-File myFile; String DEFAULT_FILE_NAME = "inject.txt";
+File injectFile;
 
-void setup() { pinMode(GREEN_LED, OUTPUT); pinMode(ORANGE_LED, OUTPUT); digitalWrite(ORANGE_LED, HIGH);
+void setup() {
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(ORANGE_LED, OUTPUT);
+  digitalWrite(ORANGE_LED, HIGH);
 
-if (!SD.begin(SD_CS)) { for (int i = 0; i < 9; i++) { digitalWrite(ORANGE_LED, HIGH); delay(100); digitalWrite(ORANGE_LED, LOW); delay(100); } return; }
+  if (!SD.begin(SD_CS_PIN)) {
+    blinkError();
+    return;
+  }
 
-myFile = SD.open(DEFAULT_FILE_NAME); if (myFile) { Keyboard.begin(); Mouse.begin();
+  injectFile = SD.open(FILENAME);
+  if (!injectFile) {
+    blinkError();
+    return;
+  }
 
-String line = ""; while (myFile.available()) { char m = myFile.read(); if (m == '\n') { processLine(line); line = ""; } else if (m != '\r') { line += m; } } if (line.length() > 0) { processLine(line); } myFile.close(); 
+  Keyboard.begin();
+  Mouse.begin();
 
+  String line = "";
+  while (injectFile.available()) {
+    char ch = injectFile.read();
+    if (ch == '\n') {
+      processLine(line);
+      line = "";
+    } else if (ch != '\r') {
+      line += ch;
+    }
+  }
+
+  if (line.length() > 0) {
+    processLine(line);
+  }
+
+  injectFile.close();
+  Keyboard.end();
+  Mouse.end();
+
+  digitalWrite(ORANGE_LED, LOW);
+  digitalWrite(GREEN_LED, HIGH);
 }
 
-Keyboard.end(); Mouse.end(); digitalWrite(ORANGE_LED, LOW); digitalWrite(GREEN_LED, HIGH); }
+void blinkError() {
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(ORANGE_LED, HIGH);
+    delay(100);
+    digitalWrite(ORANGE_LED, LOW);
+    delay(100);
+  }
+}
 
-void processLine(String line) { int spaceIndex = line.indexOf(" "); String command = (spaceIndex == -1) ? line : line.substring(0, spaceIndex); String parameters = (spaceIndex == -1) ? "" : line.substring(spaceIndex + 1);
+void processLine(String line) {
+  line.trim();
+  if (line.length() == 0) return;
 
-if (command.equals("STRING")) { Keyboard.print(parameters); } else if (command.equals("DELAY")) { delay(parameters.toInt()); } else if (command.equals("Mouse_Click_LEFT")) { Mouse.click(MOUSE_LEFT); } else if (command.equals("Mouse_Click_RIGHT")) { Mouse.click(MOUSE_RIGHT); } else if (command.startsWith("Mouse_Move_")) { int move_amount = parameters.toInt(); if (command.equals("Mouse_Move_RIGHT")) Mouse.move(move_amount, 0); else if (command.equals("Mouse_Move_LEFT")) Mouse.move(-move_amount, 0); else if (command.equals("Mouse_Move_UP")) Mouse.move(0, -move_amount); else if (command.equals("Mouse_Move_DOWN")) Mouse.move(0, move_amount); } else if (command.equals("REM")) { // Komentár } else { handleKeyCombination(command, parameters); }
+  int spaceIdx = line.indexOf(' ');
+  String cmd = (spaceIdx == -1) ? line : line.substring(0, spaceIdx);
+  String param = (spaceIdx == -1) ? "" : line.substring(spaceIdx + 1);
 
-Keyboard.releaseAll(); }
+  if (cmd == "STRING") {
+    Keyboard.print(param);
+  } else if (cmd == "DELAY") {
+    delay(param.toInt());
+  } else if (cmd == "REM") {
+    // komentár, nič sa nevykoná
+  } else if (cmd == "Mouse_Click_LEFT") {
+    Mouse.click(MOUSE_LEFT);
+  } else if (cmd == "Mouse_Click_RIGHT") {
+    Mouse.click(MOUSE_RIGHT);
+  } else if (cmd == "Mouse_Move_LEFT") {
+    Mouse.move(-param.toInt(), 0);
+  } else if (cmd == "Mouse_Move_RIGHT") {
+    Mouse.move(param.toInt(), 0);
+  } else if (cmd == "Mouse_Move_UP") {
+    Mouse.move(0, -param.toInt());
+  } else if (cmd == "Mouse_Move_DOWN") {
+    Mouse.move(0, param.toInt());
+  } else if (cmd == "CTRL" || cmd == "SHIFT" || cmd == "ALT" || cmd == "GUI") {
+    handleModifier(cmd, param);
+  } else {
+    pressKey(cmd);
+  }
 
-void handleKeyCombination(String modifier, String keys) { if (modifier.equals("ALT")) Keyboard.press(KEY_LEFT_ALT); else if (modifier.equals("CTRL")) Keyboard.press(KEY_LEFT_CTRL); else if (modifier.equals("SHIFT")) Keyboard.press(KEY_LEFT_SHIFT); else if (modifier.equals("GUI")) Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.releaseAll();
+}
 
-String key; while (keys.length() > 0) { int spaceIndex = keys.indexOf(" "); if (spaceIndex == -1) { key = keys; keys = ""; } else { key = keys.substring(0, spaceIndex); keys = keys.substring(spaceIndex + 1); } pressKey(key); }
+void handleModifier(String modifier, String keys) {
+  if (modifier == "CTRL") Keyboard.press(KEY_LEFT_CTRL);
+  else if (modifier == "SHIFT") Keyboard.press(KEY_LEFT_SHIFT);
+  else if (modifier == "ALT") Keyboard.press(KEY_LEFT_ALT);
+  else if (modifier == "GUI") Keyboard.press(KEY_LEFT_GUI);
 
-if (modifier.equals("ALT")) Keyboard.release(KEY_LEFT_ALT); else if (modifier.equals("CTRL")) Keyboard.release(KEY_LEFT_CTRL); else if (modifier.equals("SHIFT")) Keyboard.release(KEY_LEFT_SHIFT); else if (modifier.equals("GUI")) Keyboard.release(KEY_LEFT_GUI); }
+  delay(50);
 
-void pressKey(String b) { if (b.equals("ENTER")) Keyboard.press(KEY_RETURN); else if (b.equals("UP") || b.equals("UPARROW")) Keyboard.press(KEY_UP_ARROW); else if (b.equals("DOWN") || b.equals("DOWNARROW")) Keyboard.press(KEY_DOWN_ARROW); else if (b.equals("LEFT") || b.equals("LEFTARROW")) Keyboard.press(KEY_LEFT_ARROW); else if (b.equals("RIGHT") || b.equals("RIGHTARROW")) Keyboard.press(KEY_RIGHT_ARROW); else if (b.equals("DELETE")) Keyboard.press(KEY_DELETE); else if (b.equals("PAGEUP")) Keyboard.press(KEY_PAGE_UP); else if (b.equals("PAGEDOWN")) Keyboard.press(KEY_PAGE_DOWN); else if (b.equals("HOME")) Keyboard.press(KEY_HOME); else if (b.equals("ESC")) Keyboard.press(KEY_ESC); else if (b.equals("INSERT")) Keyboard.press(KEY_INSERT); else if (b.equals("TAB")) Keyboard.press(KEY_TAB); else if (b.equals("END")) Keyboard.press(KEY_END); else if (b.equals("CAPSLOCK")) Keyboard.press(KEY_CAPS_LOCK); else if (b.startsWith("F")) { int fn = b.substring(1).toInt(); if (fn >= 1 && fn <= 12) Keyboard.press(KEY_F1 + fn - 1); } else if (b.equals("SPACE")) { Keyboard.press(' '); } else { char c = b.charAt(0); uint8_t k = oemKeyToKeyCode(c); if (k != 0) Keyboard.press(k); } }
+  while (keys.length() > 0) {
+    int spaceIdx = keys.indexOf(' ');
+    String key = (spaceIdx == -1) ? keys : keys.substring(0, spaceIdx);
+    pressKey(key);
+    keys = (spaceIdx == -1) ? "" : keys.substring(spaceIdx + 1);
+  }
 
-uint8_t oemKeyToKeyCode(char c) { switch (c) { case 'a': case 'A': return 'a'; case 'b': case 'B': return 'b'; case 'c': case 'C': return 'c'; // Rozšír podľa potreby default: return 0; } }
+  delay(50);
 
-void loop() {}
+  if (modifier == "CTRL") Keyboard.release(KEY_LEFT_CTRL);
+  else if (modifier == "SHIFT") Keyboard.release(KEY_LEFT_SHIFT);
+  else if (modifier == "ALT") Keyboard.release(KEY_LEFT_ALT);
+  else if (modifier == "GUI") Keyboard.release(KEY_LEFT_GUI);
+}
+
+void pressKey(String key) {
+  key.toUpperCase();
+
+  if (key == "ENTER") Keyboard.press(KEY_RETURN);
+  else if (key == "TAB") Keyboard.press(KEY_TAB);
+  else if (key == "ESC") Keyboard.press(KEY_ESC);
+  else if (key == "SPACE") Keyboard.press(' ');
+  else if (key == "UP" || key == "UPARROW") Keyboard.press(KEY_UP_ARROW);
+  else if (key == "DOWN" || key == "DOWNARROW") Keyboard.press(KEY_DOWN_ARROW);
+  else if (key == "LEFT" || key == "LEFTARROW") Keyboard.press(KEY_LEFT_ARROW);
+  else if (key == "RIGHT" || key == "RIGHTARROW") Keyboard.press(KEY_RIGHT_ARROW);
+  else if (key == "DELETE") Keyboard.press(KEY_DELETE);
+  else if (key == "HOME") Keyboard.press(KEY_HOME);
+  else if (key == "END") Keyboard.press(KEY_END);
+  else if (key == "PAGEUP") Keyboard.press(KEY_PAGE_UP);
+  else if (key == "PAGEDOWN") Keyboard.press(KEY_PAGE_DOWN);
+  else if (key.startsWith("F")) {
+    int fn = key.substring(1).toInt();
+    if (fn >= 1 && fn <= 12) Keyboard.press(KEY_F1 + fn - 1);
+  } else if (key.length() == 1) {
+    Keyboard.press(key.charAt(0));
+  }
+}
+
+void loop() {
+  // nič sa nevykonáva
+}
